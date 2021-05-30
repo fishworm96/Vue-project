d9b400e2607707f4bcc5007c45535fd11275feab
fishworm <245525860@qq.com>
2021-05-30 21:42:09 +0800

    dist

--- dist/js/chunk-2d0aad65.fefee727.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aad65"],{"135b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" I am parent "),t("router-view")],1)},r=[],u={},c=u,l=t("2877"),i=Object(l["a"])(c,a,r,!1,null,null,null);n["default"]=i.exports}}]);

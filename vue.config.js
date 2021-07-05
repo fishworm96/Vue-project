6ffc0a19f011be81244bc294ac95527a0cacbe7e
fishworm96 <245525860@qq.com>
2021-07-05 15:02:37 +0800

    优化首页加载速度

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
 
 const resolve = dir => path.join(__dirname, dir)
-
-
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   outputDir: 'dist',
@@ -43,5 +45,23 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack : {
+    plugins: [
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      
+      // 配置compression-webpack-plugin压缩
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5, 
+        minChunkSize: 100
+      })
+    ]
   }
 }
